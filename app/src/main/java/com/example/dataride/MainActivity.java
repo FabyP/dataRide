bd220fd22dc5ea0ec5fdb0931113666efb895921
Mayra <mayra.fahrer@iem.thm.de>
2019-12-10 18:09:17 +0100

    Nmea Filter auf Qualität

--- app/src/main/java/com/example/dataride/MainActivity.java
+++ app/src/main/java/com/example/dataride/MainActivity.java
@@ -32,6 +32,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class MainActivity extends AppCompatActivity implements LocationListener, OnNmeaMessageListener {
     // minimale zeit und distanz ab der die gps daten aktualisiert werden
     //angegeben in milli sekunden
@@ -42,7 +43,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
     private static final float MIN_DISTANCE_TO_REFRESH = 0F;
 
     LocationManager lm = null;
-    OnNmeaMessageListener nmeaListener = null;
+    OnNmeaMessageListener nmeaListener;
     LocationListener gpsListener = null;
 
     //Testzwecken
@@ -53,6 +54,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
 
     FloatingActionButton fab;
     boolean clicked;
+    boolean gpsQuality;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,8 +63,11 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         BottomNavigationView navView = findViewById(R.id.nav_view);
         textLat = (TextView) findViewById(R.id.textView);
         textLong = (TextView) findViewById(R.id.textView2);
+        textNmea = (TextView) findViewById(R.id.textView3);
+        text4 = (TextView) findViewById(R.id.textView4);
         fab = (FloatingActionButton) findViewById(R.id.fab);
         clicked = false;
+        gpsQuality = false;
 
 
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -77,17 +82,17 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
             @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View v) {
-                if(clicked){
-                    Toast.makeText(MainActivity.this,"Stop Tracking",Toast.LENGTH_SHORT).show();
-                    Log.v("CLICKED","");
-                    stopTracking();
-                    clicked = false;
-                } else{
-                    startTracking();
-                    Toast.makeText(MainActivity.this,"Start Tracking",Toast.LENGTH_SHORT).show();
-                    Log.v("CLICKED","");
-                    clicked = true;
-                }
+                //if(clicked){
+                    //Toast.makeText(MainActivity.this,"Stop Tracking",Toast.LENGTH_SHORT).show();
+                    //Log.v("CLICKED","");
+                   //stopTracking();
+                    //clicked = false;
+                //} else{
+                        startTracking();
+                        Toast.makeText(MainActivity.this,"Start Tracking",Toast.LENGTH_SHORT).show();
+                        Log.v("CLICKED","");
+                 //   clicked = true;
+                //}
 
             }
         });
@@ -101,16 +106,20 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         }
         LocationManager lm = (LocationManager) MainActivity.this.getSystemService(Context.LOCATION_SERVICE);
         if(lm != null) {
-            lm.addNmeaListener(MainActivity.this);
             boolean isGPSEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
             if (isGPSEnabled) {
                 lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000, 0, MainActivity.this);
+                Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                onLocationChanged(location);
+                lm.addNmeaListener(MainActivity.this);
             } else {
                 textLong.setText("SHIT");
             }
         } else{
             Toast.makeText(MainActivity.this, "Kein Signal gefunden", Toast.LENGTH_LONG).show();
         }
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000, 0, MainActivity.this);
+        lm.addNmeaListener(MainActivity.this);
     }
     public  void stopTracking(){
         //hier sicherstellen das er alles vorherige rausnimmt
@@ -118,40 +127,18 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         textLong.setText("Remove");
     }
 
-    //Android Lifecycle Methoden
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        lm.removeUpdates(MainActivity.this);
-    }
-
-    //wird immer wieder aufgerufen auch wenn die app geschlossen war aber noch nicht gelöscht wurde,
-    // deswegen hier nochmal der check ob gps immer noch vorhanden ist
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    //hier wird alles entfernt sobald der Nutzer die app schließt
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        lm.removeUpdates(MainActivity.this);
-    }
-
     @Override
     public void onLocationChanged(Location location) {
         if(location != null) {
-        double lat = location.getLatitude();
-        double lon = location.getLongitude();
-        String latString = Double.toString(lat);
-        String lonString = Double.toString(lon);
-        textLat.setText(latString);
+            double lat = location.getLatitude();
+            double lon = location.getLongitude();
+            String latString = Double.toString(lat);
+            String lonString = Double.toString(lon);
+            textLat.setText(latString);
+            textLong.setText(lonString);
+
+        } else{
+            textLat.setText("geht nicht");
         }
     }
 
@@ -170,8 +157,79 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
 
     }
 
+    //Android Lifecycle Methoden
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //lm.removeUpdates(MainActivity.this);
+    }
+
+    //wird immer wieder aufgerufen auch wenn die app geschlossen war aber noch nicht gelöscht wurde,
+    // deswegen hier nochmal der check ob gps immer noch vorhanden ist
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    //hier wird alles entfernt sobald der Nutzer die app schließt
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //lm.removeUpdates(MainActivity.this);
+    }
+
     @Override
     public void onNmeaMessage(String message, long timestamp) {
-        textLong.setText(message);
+        processNmea(message);
+    }
+    public void processNmea(String nmea){
+        boolean GSAGood = false;
+        boolean GGAGood = false;
+        boolean GSVGood = false;
+
+        String[] rawNmeaSplit = nmea.split(",");
+
+        if (rawNmeaSplit[0].equalsIgnoreCase("$GPGGA")){
+            String fixQualityString = rawNmeaSplit[6];
+            Integer fixQuality = Integer.parseInt(fixQualityString);
+            String numberSatellitesString = rawNmeaSplit[7];
+            Integer numberSatellites = Integer.parseInt(numberSatellitesString);
+            if(fixQuality > 0 && fixQuality < 5 && numberSatellites > 4){
+                GGAGood = true;
+            } else{
+                GGAGood = false;
+            }
+        }
+        if(GGAGood) {
+            if (rawNmeaSplit[0].equalsIgnoreCase("$GPGSA")) {
+                String PDOPString = rawNmeaSplit[15];
+                Double PDOP = Double.parseDouble(PDOPString);
+                if (PDOP < 6) {
+                    GSAGood = true;
+                } else {
+                    GSAGood = false;
+                }
+
+            }
+            if (GSAGood) {
+                for (int i = 0; i < 4; i++) {
+                    if (rawNmeaSplit[0].equalsIgnoreCase("$GPGSV")) {
+                        String SNRString = rawNmeaSplit[0];
+                        Integer SNR = Integer.parseInt(SNRString);
+                        if (SNR > 30) {
+                            GSVGood = true;
+                        }
+                    }
+                }
+            }
+        }
+        if(GGAGood && GSAGood && GSVGood){
+            gpsQuality = true;
+        }
     }
 }
